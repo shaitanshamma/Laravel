f6661022093ba55ff72e41850d8010eff40a2986
shamma <shammacode@gmail.com>
2022-01-16 10:16:30 +0300

    Lesson 2 task

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+
+    public function news()
+    {
+        $news = $this->getNews();
+        return view('/news/news', ['newsList'=>$news]);
+    }
+
+    public function showNews(int $id)
+    {
+        $news = $this->showNewsById($id);
+        return view('/news/customNews', ['news'=>$news]);
+    }
+}
---
+++ resources/views/admin/categories.php
@@ -0,0 +1,9 @@
+<?php foreach ($categoryList as $category): ?>
+
+<div>
+    <p><?=$category['id']?></p>
+    <p><?=$category['title']?></p>
+</div>
+    <hr>
+
+<?php endforeach;?>
